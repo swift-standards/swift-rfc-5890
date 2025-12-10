e77d9bab2179287d6b2839ad297eaeb4c5317f16
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 08:27:11 +0100

    Update dependencies to latest versions

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-rfc-3492", from: "0.1.0"),
+        .package(url: "https://github.com/swift-standards/swift-rfc-3492", from: "0.1.2"),
     ],
     targets: [
         .target(
